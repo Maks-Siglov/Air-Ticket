4749f372e63c1be921551154b02f1f07f9b25c64
Maks-Siglov <gmb333x2@gmail.com>
2024-02-29 18:32:32 +0200

    Login, Register provided

---
+++ air_crm/users/forms/__init__.py
@@ -0,0 +1,1 @@
+from users.forms.register import RegisterForm
--- air_crm/users/views.py
+++ air_crm/users/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from users.forms import RegisterForm
+
+
+def register(request: HttpRequest) -> HttpResponse | HttpResponseRedirect:
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account successfully register")
+            return redirect("users:login")
+    else:
+        form = RegisterForm()
+
+    return render(
+        request, "users/register.html", {"form": form}
+    )
+
+
+def login(request: HttpRequest) -> HttpResponseRedirect:
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+
+            user = auth.authenticate(
+                request, username=email, password=password
+            )
+            if user is not None:
+                auth.login(request, user)
+                messages.success(request, "You are log in")
+                return redirect("main:index")
+    else:
+        form = AuthenticationForm()
+
+    return render(
+        request, "users/login.html", {"form": form}
+    )
