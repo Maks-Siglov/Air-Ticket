3f3b73c7ee1d81e5ac0694c60fa95bf9622995c6
Maks-Siglov <gmb333x2@gmail.com>
2024-03-06 17:12:20 +0200

    Change password provided

--- air_crm/users/urls.py
+++ air_crm/users/urls.py
@@ -7,5 +7,6 @@ app_name = "users"
 urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", views.login, name="login"),
-    path("logout/", views.logout, name="logout")
+    path("logout/", views.logout, name="logout"),
+    path("change-password/", views.change_password, name="change_password"),
 ]
--- air_crm/users/views.py
+++ air_crm/users/views.py
@@ -1,6 +1,7 @@
 from django.contrib import auth, messages
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 
@@ -45,3 +46,19 @@ def logout(request: HttpRequest) -> HttpResponseRedirect:
     auth.logout(request)
     messages.success(request, "You are logged out")
     return redirect("main:index")
+
+
+@login_required(login_url="users:login")
+def change_password(request: HttpRequest):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, "You password successfully changed.")
+            return redirect("customer:profile")
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(
+        request, "users/change_password.html", {"form": form}
+    )
