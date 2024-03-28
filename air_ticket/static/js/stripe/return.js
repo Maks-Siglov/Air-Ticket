1c822bbdddeae525264c8dba57ce2c28842619a8
Maks-Siglov <gmb333x2@gmail.com>
2024-03-28 20:55:10 +0200

    Minor js change

--- air_ticket/static/js/stripe/return.js
+++ air_ticket/static/js/stripe/return.js
@@ -1,15 +1,12 @@
-
 initialize();
 
 async function initialize() {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const sessionId = urlParams.get('session_id');
-  const url = `http://localhost:8000/orders/session-status?session_id=${sessionId}`
+  const url = `http://localhost:8000/orders/session-status/?session_id=${sessionId}`
   const response = await fetch(url);
   const session = await response.json();
-
-  console.log(session.status)
 
   if (session.status == 'open') {
     window.replace('http://localhost:8000/orders/checkout')
