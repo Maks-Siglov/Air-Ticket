44b15ff1c0b8244d5fb00080076c9c4eb4491d46
Maks-Siglov <gmb333x2@gmail.com>
2024-03-29 15:34:02 +0200

    Check in seat map pagination. Firs version provided

--- air_ticket/static/js/seat_map/map.js
+++ air_ticket/static/js/seat_map/map.js
@@ -2,41 +2,92 @@ import {handleSeatSelection} from './select_seat.js';
 import {handleCancelSeat} from "./cancel_seat.js";
 
 const seatMapContainer = document.getElementById('seat-map-container');
-
 const seatImageUrl = '/static/images/seat.png';
-seatMapContainer.textContent = 'Loading seat map data...';
+
+const seatsPerPage = 10;
+let currentPage = 1;
 
 
-fetch('/api/v1/check-in/' + flightPk)
-    .then(response => response.json())
-    .then(seatsData => {
-        seatMapContainer.textContent = '';
+function generateSeatMap() {
+    seatMapContainer.textContent = 'Loading seat map data...';
 
-        const seatsAmount = seatsData.length;
-        const rows = Math.floor(Math.sqrt(seatsAmount));
-        const columns = 15
+    fetch('/api/v1/check-in/' + flightPk)
+        .then(response => response.json())
+        .then(seatsData => {
+            seatMapContainer.textContent = '';
 
-        for (let row = 0; row < rows; row++) {
-            const rowElement = document.createElement('div');
-            rowElement.classList.add(`seat-row-${row}`, 'd-flex', 'justify-content-center');
+            const seatsAmount = seatsData.length;
+            const totalPages = Math.ceil(seatsAmount / seatsPerPage);
 
-            for (let col = 0; col < columns; col++) {
-                const seatIndex = row * columns + col;
-                if (seatIndex < seatsData.length) {
-                    const seat = seatsData[seatIndex];
-                    const seatElement = createSeatElement(seat)
+            const startIndex = (currentPage - 1) * seatsPerPage;
+            const endIndex = Math.min(startIndex + seatsPerPage, seatsAmount);
 
-                    seatElement.addEventListener('click', handleSeatSelection);
+            const rows = 3;
+            const columns =  Math.ceil(seatsPerPage / rows);
 
-                    rowElement.appendChild(seatElement);
+            for (let row = 0; row < rows; row++) {
+                const rowElement = createRowElement(row)
+
+                for (let col = 0; col < columns; col++) {
+                    const seatIndex = row * columns + col + startIndex;
+                    if (seatIndex < endIndex) {
+                        const seat = seatsData[seatIndex];
+                        const seatElement = createSeatElement(seat)
+
+                        seatElement.addEventListener('click', handleSeatSelection);
+
+                        rowElement.appendChild(seatElement);
+                    }
                 }
-            }
 
-            seatMapContainer.appendChild(rowElement);
-        }
-        displaySelectedUserSeat();
-    })
-    .catch(error => console.error(error));
+                seatMapContainer.appendChild(rowElement);
+            }
+            displaySelectedUserSeat();
+
+            generatePagination(currentPage, totalPages);
+        })
+        .catch(error => console.error(error));
+}
+
+
+function generatePagination(currentPage, totalPages){
+    const paginationContainer = document.getElementById('pagination-container');
+    paginationContainer.innerHTML = '';
+
+    const previousButton = document.createElement('button');
+    previousButton.id = 'previous-page-btn';
+    previousButton.className = 'btn btn-secondary mx-2';
+    previousButton.textContent = 'Previous';
+    previousButton.addEventListener('click', () => previousPage());
+
+    const currentPageSpan = document.createElement('span');
+    currentPageSpan.id = 'current-page';
+    currentPageSpan.textContent = `Page ${currentPage} of ${totalPages}`;
+
+    const nextButton = document.createElement('button');
+    nextButton.id = 'next-page-btn';
+    nextButton.className = 'btn btn-secondary mx-2';
+    nextButton.textContent = 'Next';
+    nextButton.addEventListener('click', () => nextPage(totalPages));
+
+    paginationContainer.appendChild(previousButton);
+    paginationContainer.appendChild(currentPageSpan);
+    paginationContainer.appendChild(nextButton);
+}
+
+function nextPage(totalPages) {
+    if (currentPage < totalPages) {
+        currentPage++;
+        generateSeatMap();
+    }
+}
+
+function previousPage() {
+    if (currentPage > 1) {
+        currentPage--;
+        generateSeatMap();
+    }
+}
 
 function createSeatElement(seat) {
     const seatElement = document.createElement('div');
@@ -60,6 +111,12 @@ function createSeatElement(seat) {
     return seatElement;
 }
 
+function createRowElement(row){
+    const rowElement = document.createElement('div');
+    rowElement.classList.add(`seat-row-${row}`, 'd-flex', 'justify-content-center');
+    return rowElement
+}
+
 function displaySelectedUserSeat() {
     if (approvedSelectedSeatsIDs.length) {
         approvedSelectedSeatsIDs.forEach(seatId => {
@@ -73,3 +130,7 @@ function displaySelectedUserSeat() {
         });
     }
 }
+
+generateSeatMap();
+document.getElementById('previous-page-btn').addEventListener('click', previousPage);
+document.getElementById('next-page-btn').addEventListener('click', nextPage);
