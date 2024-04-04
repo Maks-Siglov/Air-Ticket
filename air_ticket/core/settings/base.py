fcc003e68ee8199ef833641cbc964e98a7128906
Maks-Siglov <gmb333x2@gmail.com>
2024-04-04 21:40:01 +0300

    Minor settings improvement

--- air_ticket/core/settings/base.py
+++ air_ticket/core/settings/base.py
@@ -97,6 +97,14 @@ DATABASES = {
     }
 }
 
+# Rest framework
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ]
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
