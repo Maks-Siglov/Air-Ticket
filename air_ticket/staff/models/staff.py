87b3a5660155e9d5c31ef45552fc7df967dfd77d
Maks-Siglov <gmb333x2@gmail.com>
2024-04-15 10:49:59 +0300

    Minor staff model improvement

--- air_ticket/staff/models/staff.py
+++ air_ticket/staff/models/staff.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Staff(models.Model):
-    user = models.OneToOneField("users.User", on_delete=models.CASCADE)
-    role = models.ForeignKey("staff.Role", on_delete=models.CASCADE)
+    user = models.OneToOneField("users.User", on_delete=models.DO_NOTHING)
+    role = models.ForeignKey("staff.Role", on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return f"{self.user} {self.role}"
